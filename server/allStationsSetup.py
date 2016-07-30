87b0b98ce4361a14478b9a63edbbbdba1c72af69
Michel Jordan Valencia Rangel <ascherm97@gmail.com>
2016-07-30 17:08:09 -0500

    Add the functionality for distance matrix API

--- server/allStationsSetup.py
+++ server/allStationsSetup.py
@@ -13,16 +13,25 @@ session = DBSession()
 names = ["Indios Verdes","Deportivo 18 De Marzo","Euzkaro","Potrero","La Raza","Circuito","San Simon","Manuel Gonzalez","Buenavista"]
 latitudes = [19.4966275,19.48636,19.48247,19.47661,19.46883,19.46244,19.45966,19.45688,19.44695]
 longitudes = [-99.1197621,-99.12432,-99.12774,-99.13247,-99.1388,-99.14403,-99.14651,-99.14915,-99.15302]
+urls = [
+	"",
+    "http://3.bp.blogspot.com/-GUayYNNMLYM/TglkJaXmR7I/AAAAAAAABEo/EvAno18rYZg/s400/mb-indiosverdes.png",
+    "http://1.bp.blogspot.com/_XXpvlSXUPdU/TQMmDQXiy_I/AAAAAAAAApA/_XeO0Q5Gs7E/s400/mb-18marzo.png",
+    "http://4.bp.blogspot.com/-ci4pEm1Untc/TYl3mzppBfI/AAAAAAAAA48/8DNPNJghtIE/s400/mb-euzkaro.png",
+    "http://3.bp.blogspot.com/-BpbYjDmpc0k/Tj-A8seLceI/AAAAAAAABUI/yiPHWd5N2U8/s400/mb-potrero.png",
+    "http://3.bp.blogspot.com/-LGGdFFx46Cc/TWiul15ZxJI/AAAAAAAAAzg/pw2nIEd4PIQ/s400/mb-laraza.png",
+    "http://1.bp.blogspot.com/_XXpvlSXUPdU/TOS-LiIsj8I/AAAAAAAAAeo/Oa0_JXrf_4w/s400/mb-circuito.png",
+    "http://4.bp.blogspot.com/-ZlgbfnCe_98/T0r4PjbtgjI/AAAAAAAACX0/5lfhwVgXsmg/s400/mb-sansimon.png",
+    "http://4.bp.blogspot.com/-RGCLj4rbBiI/TmrkVbzWKGI/AAAAAAAABiE/O4-1mlqhzR4/s1600/mb-manuelgonzalez.png",
+    "http://lh3.ggpht.com/_XXpvlSXUPdU/TODg-7JLnRI/AAAAAAAAAbg/rdSFDi-icgU/s400/mb-buenavista.png"
+]
 
 for i in range(0,9):
 	station = Station(longitude = longitudes[i],
                 latitude = latitudes[i],
                 name = names[i],
-                url_img  = "")
+                url_img  = urls[i])
 	session.add(station)
 	session.commit()
 
 print "Finish loading defalut stations"
-
-
-
--- server/server.py
+++ server/server.py
@@ -9,6 +9,51 @@ import httplib2
 from flask import make_response
 import requests
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 app = Flask(__name__)
 
@@ -31,12 +76,14 @@ def indexForStation():
 
 # Endpoint for buses data
 @app.route('/json/buses')
+@crossdomain(origin='*')
 def busesJSON():
     buses = session.query(Bus).all()
     return jsonify(buses = [b.serialize for b in buses])
 
 # Endpoint for stations data
 @app.route('/json/stations')
+@crossdomain(origin='*')
 def stationsJSON():
     stations = session.query(Station).all()
     return jsonify(stations = [s.serialize for s in stations])
@@ -54,4 +101,18 @@ def newBus():
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 8080)
+
+def getETABetween(locationOne, locationTwo):
+    google_key_secret ="AIzaSyDafqyfJNYmaMFDciA8WESdmulQ5dObn_U"
+
+    url = ("https://maps.googleapis.com/maps/api/distancematrix/json?"+
+        "origins=%s,%s&destinations=%s,%s&mode=driving"+
+        "&language=es-ES&key=%s" % (locationOne[0], locationOne[1],
+        locationOne[0], locationOne[1], google_key_secret))
+    h = httplib2.Http()
+    response, content = h.request(url, 'GET')
+    result = json.loads(content)
+    eta = result["rows"][0]["elements"][0]["duration"]["value"]
+    print str(eta)
+    return 1
 
